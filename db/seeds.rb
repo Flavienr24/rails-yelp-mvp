f17c5f67931205030967a80f7f57432193eb38f2
Flavien Rosellini <rosellini.flavien@gmail.com>
2019-11-20 20:47:29 +0100

    Yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,42 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Knowhere',
+    address:      'La Capte, Hyères',
+    category:  'japanese',
+    phone_number:        '06 65 83 25 40'
+  },
+  {
+    name:         'Le Gio',
+    address:      'Rue de la plage, Sanary',
+    category:  'french',
+    phone_number:        '06 65 83 25 40'
+  },
+  {
+    name:         'La frite folle',
+    address:      'Avenue de Bruxelle',
+    category:  'belgian',
+    phone_number:        '06 65 83 25 40'
+  },
+  {
+    name:         'La mama della pasta',
+    address:      'Via Palacopadimonda',
+    category:  'italian',
+    phone_number:        '06 65 83 25 40'
+  },
+  {
+    name:         'Ping Pong',
+    address:      'Boulevard de Shanghai',
+    category:  'chinese',
+    phone_number:        '06 65 83 25 40'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
+
